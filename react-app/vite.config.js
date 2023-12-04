415cb02b87c7002975d7b385b7b690bd66d8b06c
John Paxton <johnpaxton@gmail.com>
2023-12-04 11:31:41 -0500

    Exclude backups

--- react-app/vite.config.js
+++ react-app/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { configDefaults } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,5 +9,6 @@ export default defineConfig({
 		globals: true,
 		environment: 'jsdom',
 		setupFiles: './tests/setup.js',
+		exclude: [...configDefaults.exclude, '**/backups/**'],
 	},
 });
