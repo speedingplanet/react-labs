9d2765b28fabae951169b1d673597e19724285ad
John Paxton <johnpaxton@gmail.com>
2023-12-04 09:17:46 -0500

    More flexible Lab manager

--- labs/finished/lab-01/LabManager.jsx
+++ labs/finished/lab-01/LabManager.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import './LabManager.css';
 import Calculator from './Calculator';
+import CalculatorFinished from './finished/Calculator';
+import { Link, Route, Routes } from 'react-router-dom';
 
 const LabManager = () => {
 	return (
 		<>
-			<h2>Lab 1: Calculator</h2>
+			<h2>Lab 1: Calculator (finished)</h2>
+			<nav className="navbar">
+				<ul>
+					<li>
+						<Link to="/labs/yours">Yours</Link>
+					</li>
+					<li>
+						<Link to="/labs/finished">Finished</Link>
+					</li>
+				</ul>
+			</nav>
 			<section className="lab-container">
-				<Calculator />
+				<Routes>
+					<Route
+						path="/yours"
+						element={<Calculator />}
+					/>
+					<Route
+						path="/finished"
+						element={<CalculatorFinished />}
+					/>
+				</Routes>
 			</section>
 		</>
 	);
