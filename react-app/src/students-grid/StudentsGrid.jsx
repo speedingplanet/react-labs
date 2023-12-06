4618f5d89b546dfe7df77b094b00172809fe122a
John Paxton <johnpaxton@gmail.com>
2023-12-06 14:23:32 -0500

    Grid update 1

--- react-app/src/students-grid/StudentsGrid.jsx
+++ react-app/src/students-grid/StudentsGrid.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import './StudentsGrid.css';
 
 function StudentsGrid() {
-	return <div>Just a placeholder at the moment.</div>;
+	return (
+		<section className="students-grid">
+			<div className="students-grid-cell">First Name</div>
+			<div className="students-grid-cell">Last Name</div>
+			<div className="students-grid-cell">John</div>
+			<div className="students-grid-cell">Paxton</div>
+			<div className="students-grid-cell">Andreina</div>
+			<div className="students-grid-cell">Castillo</div>
+		</section>
+	);
 }
 
 export default StudentsGrid;
