e97938bcc780bfc5625b2f4b8607424b5808e9d6
John Paxton <johnpaxton@gmail.com>
2023-12-06 16:44:29 -0500

    Dynamically rendered students

--- react-app/src/students-grid/StudentsGrid.jsx
+++ react-app/src/students-grid/StudentsGrid.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import { students } from '../data/students.json';
 import './StudentsGrid.css';
 
 function StudentsGrid() {
+	console.log(`There are ${students.length} students`);
+
 	return (
 		<section className="students-grid">
-			<div className="students-grid-cell">First Name</div>
-			<div className="students-grid-cell">Last Name</div>
-			<div className="students-grid-cell">John</div>
-			<div className="students-grid-cell">Paxton</div>
-			<div className="students-grid-cell">Andreina</div>
-			<div className="students-grid-cell">Castillo</div>
+			<div className="students-grid-cell header-cell">First Name</div>
+			<div className="students-grid-cell header-cell">Last Name</div>
+			{students.map((student) => {
+				return (
+					<div
+						className="students-grid-row"
+						key={student.id}
+					>
+						<div className="students-grid-cell">{student.firstName}</div>
+						<div className="students-grid-cell">{student.lastName}</div>
+					</div>
+				);
+			})}
 		</section>
 	);
 }
