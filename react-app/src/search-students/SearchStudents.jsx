37409fbdb90430e24585a66164af1cabc2336f30
John Paxton <johnpaxton@gmail.com>
2023-12-07 18:22:21 -0500

    Update SearchStudents

--- react-app/src/search-students/SearchStudents.jsx
+++ react-app/src/search-students/SearchStudents.jsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+/*
+Searching on address.country
+What URL / address are we querying?
+Gather search criteria from the form
+Send it to the API
+Do we tell the API to sort?
+
+Part One:
+Design and implement a form with a drop-down of three countries 
+(US, UK, Canada) And a button labeled "Search".
+
+When the user clicks on the Search button, log to the console
+which country they searched on.
+
+*/
 
 function SearchStudents() {
+	const [country, setCountry] = useState('');
+
+	function handleSearchButton() {
+		console.log(`You want to search on ${country}`);
+	}
+
+	function handleSelectCountry(event) {
+		setCountry(event.currentTarget.value);
+	}
+
 	return (
 		<div>
 			<h3>SearchStudents</h3>
-			<p>Placeholder for now</p>
+			<div>
+				<label htmlFor="country">Choose a country:</label>
+				<select
+					name="country"
+					id="country"
+					value={country}
+					onChange={handleSelectCountry}
+				>
+					<option value="">Countries:</option>
+					<option>United States</option>
+					<option>Canada</option>
+					<option>United Kingdom</option>
+				</select>
+				<button
+					type="button"
+					onClick={handleSearchButton}
+				>
+					Search
+				</button>
+			</div>
 		</div>
 	);
 }
