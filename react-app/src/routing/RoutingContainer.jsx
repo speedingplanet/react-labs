622df24967ac0ad4efd9db7ead634a9a17bdf7f8
John Paxton <johnpaxton@gmail.com>
2023-12-08 17:51:06 -0500

    More routing

--- react-app/src/routing/RoutingContainer.jsx
+++ react-app/src/routing/RoutingContainer.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './RoutingContainer.css';
+import { Link, Route, Routes } from 'react-router-dom';
 
 function RoutingContainer() {
 	return (
@@ -8,11 +9,50 @@ function RoutingContainer() {
 				<h3>Routing Experiments</h3>
 			</header>
 			<div className="routing-container">
-				<nav className="left"></nav>
-				<div className="right"></div>
+				<nav className="left">
+					<ul>
+						<li>
+							<Link to="/routing/first">First</Link>
+						</li>
+						<li>
+							<Link to="/routing/second">Second</Link>
+						</li>
+						<li>
+							<Link to="/routing/third">Third</Link>
+						</li>
+					</ul>
+				</nav>
+				<div className="right">
+					<Routes>
+						<Route
+							path="first"
+							element={<First />}
+						/>
+						<Route
+							path="second"
+							element={<Second />}
+						/>
+						<Route
+							path="third"
+							element={<Third />}
+						/>
+					</Routes>
+				</div>
 			</div>
 		</>
 	);
 }
 
 export default RoutingContainer;
+
+function First() {
+	return <div>First</div>;
+}
+
+function Second() {
+	return <div>Second</div>;
+}
+
+function Third() {
+	return <div>Third</div>;
+}
