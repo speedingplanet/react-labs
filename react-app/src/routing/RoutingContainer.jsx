3b2c7f07c8eddbb6c7a1597a3a1cdddd5d94e136
John Paxton <johnpaxton@gmail.com>
2023-12-08 17:44:44 -0500

    Routing layout

--- react-app/src/routing/RoutingContainer.jsx
+++ react-app/src/routing/RoutingContainer.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import './RoutingContainer.css';
 
 function RoutingContainer() {
-	return <div>RoutingContainer</div>;
+	return (
+		<>
+			<header>
+				<h3>Routing Experiments</h3>
+			</header>
+			<div className="routing-container">
+				<nav className="left"></nav>
+				<div className="right"></div>
+			</div>
+		</>
+	);
 }
 
 export default RoutingContainer;
